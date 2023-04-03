dde2158faecb4b986a8a93c40d4f7a742128089e
Wic47 <116346023+Wic47@users.noreply.github.com>
2023-04-03 08:36:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const canvas = document.getElementById("canvas")
+const c = canvas.getContext("2d")
+const width = canvas.width
+const height = canvas.height
+window.focus
+
+const ship =  {
+    x: width/2,
+    y: height/2,
+    dy: 3,
+    dx: 3,
+    movement: {
+        forward: false,
+        right: false,
+        left: false
+    },
+    draw() {
+        c.font = "30px Arial"
+        c.fillStyle = "white"
+        c.fillText("A", ship.x, ship.y)
+    }
+}
+
+document.addEventListener("keydown", e => {
+    const key = e.key
+    
+    if (key == "w") {
+        ship.movement.forward = true
+    }
+    else if (key == "a") {
+        ship.movement.left = true
+    }
+    else if (key == "d") {
+        ship.movement.right = true
+    }
+})
+
+document.addEventListener("keyup", e => {
+    const key = e.key
+    
+    if (key == "w") {
+        ship.movement.forward = false
+    }
+    else if (key == "a") {
+        ship.movement.left = false
+    }
+    else if (key == "d") {
+        ship.movement.right = false
+    }
+})
+
+function draw() {
+    requestAnimationFrame(draw)
+    if (ship.movement.left) {
+        c.translate(ship.x,ship.y)
+        c.rotate(-0.05)
+        c.translate(-ship.x,-ship.y)
+    }
+    if (ship.movement.right) {
+        c.translate(ship.x,ship.y)
+        c.rotate(0.05)
+        c.translate(-ship.x,-ship.y)
+    }
+    c.save()
+    c.setTransform(1,0,0,1,0,0)
+    c.clearRect(0,0,width,height)
+    c.restore()
+    ship.draw()
+
+    if (ship.movement.forward) {
+        ship.y -= ship.dy
+    }
+}   
+
+draw()
